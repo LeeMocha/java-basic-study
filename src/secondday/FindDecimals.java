ebb45b61034fb6b68b99630e49ff0ef554442857
LeeMocha <qwertui1217@gmail.com>
2022-06-12 12:17:01 +0900

    FindDecimals.java import 오류 수정

--- src/secondday/FindDecimals.java
+++ src/secondday/FindDecimals.java
@@ -1,7 +1,5 @@
 package secondday;
 
-import study.exampletutorial.포와와일문.WhileTest1;
-
 import java.util.Scanner;
 
 public class FindDecimals {
@@ -27,15 +25,15 @@ public class FindDecimals {
             }
             System.out.println("계속 하시겠습니까? 1:예  2:아니오");
             int num2 = scanner.nextInt();
-                if (num2 == 1) {
-                    continue;
-                } else if (num2 == 2) {
-                    System.out.println("종료하겠습니다");
-                    break;
-                } else {
-                    System.out.println("보기에 있는 숫자만 쓰시오.");
-                    //바로 위 "계속 하시겠습니까?"로 못넘어가는게 아쉽다.
-                }
+            if (num2 == 1) {
+                continue;
+            } else if (num2 == 2) {
+                System.out.println("종료하겠습니다");
+                break;
+            } else {
+                System.out.println("보기에 있는 숫자만 쓰시오.");
+                //바로 위 "계속 하시겠습니까?"로 못넘어가는게 아쉽다.
             }
         }
     }
+}
