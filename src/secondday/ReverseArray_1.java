ac771f067900b1b970dbb036364443d21f57cf9f
LeeMocha <qwertui1217@gmail.com>
2022-06-12 12:18:18 +0900

    #master add ReverseArray1.java

---
+++ src/secondday/ReverseArray_1.java
@@ -0,0 +1,27 @@
+package secondday;
+
+import java.util.Scanner;
+
+public class ReverseArray_1 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[5];
+        System.out.println("---- 배열 입력받은 반대로 출력하기 ----");
+        System.out.println("1번째 숫자: ");
+        arr[4] = scanner.nextInt();
+        System.out.println("2번째 숫자: ");
+        arr[3] = scanner.nextInt();
+        System.out.println("3번째 숫자: ");
+        arr[2] = scanner.nextInt();
+        System.out.println("4번째 숫자: ");
+        arr[1] = scanner.nextInt();
+        System.out.println("5번째 숫자: ");
+        arr[0] = scanner.nextInt();
+        System.out.println("꺼꾸로 숫자 출력하기");
+        System.out.println(arr[0]);
+        System.out.println(arr[1]);
+        System.out.println(arr[2]);
+        System.out.println(arr[3]);
+        System.out.println(arr[4]);
+    }
+}
